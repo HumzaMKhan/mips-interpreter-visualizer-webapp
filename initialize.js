797156ceaceb41e56113ad26e8ce58c3ab1144da
Humza Khan <humzamk@uci.edu>
2020-10-08 23:30:06 -0700

    Migrated script to initialize.js

---
+++ initialize.js
@@ -0,0 +1,43 @@
+"use strict"
+
+import {
+    updateMemoryTable, updateRegisterTable,
+    previousMemory, nextMemory, showAddress, showTextSegment, showDataSegment, showStack, showGlobal,
+    assemble, reset, restart, run, step
+} from './processor.js';
+
+// add line numbers to the textareas
+$(document).ready(function(){
+    $('.lined').linedtextarea();
+});
+
+// flip the disabled attribute of every button in buttons
+function flipButtons(...buttons) {
+    for(let i = 0, buttonsLength = buttons.length; i < buttonsLength; i++) document.getElementById(buttons[i]).disabled = !document.getElementById(buttons[i]).disabled;
+}
+
+// initially display memory and registers
+updateMemoryTable();
+updateRegisterTable();
+
+// disable buttons which need assemble to be pressed first
+flipButtons('reset', 'restart', 'run', 'step');
+
+// add functionality to all of the buttons
+document.querySelector('#restart').addEventListener('click', restart);
+document.querySelector('#run').addEventListener('click', run);
+document.querySelector('#step').addEventListener('click', step);
+document.querySelector('#previousMemory').addEventListener('click', previousMemory);
+document.querySelector('#nextMemory').addEventListener('click', nextMemory);
+document.querySelector('#textSegment').addEventListener('click', showTextSegment);
+document.querySelector('#dataSegment').addEventListener('click', showDataSegment);
+document.querySelector('#stackPointer').addEventListener('click', showStack);
+document.querySelector('#globalPointer').addEventListener('click', showGlobal);
+document.querySelector('#assemble').addEventListener('click', function() {
+    assemble(document.getElementById('code').value);            // assemble the input code
+    flipButtons('assemble', 'reset', 'restart', 'run', 'step'); // enable buttons which need assemble to be pressed first
+});
+document.querySelector('#reset').addEventListener('click', function() {
+    flipButtons('assemble', 'reset', 'restart', 'run', 'step'); // allow assemble to be pressed, but not buttons which need assemble to be pressed first
+    reset()
+});
