1f08648291f2c2c087602b26eb33b75a10c80987
Humza Khan <humzamk@uci.edu>
2020-10-11 22:51:12 -0700

    Finished data section memory parsing

--- parser.js
+++ parser.js
@@ -21,7 +21,11 @@ function parseCode(code) {
     // find data segment
     let dataLocation = code.search(/\.data/i);
     let dataMemory = {};
-    if(dataLocation != -1) dataMemory = dataToMemory(code.slice(dataLocation + 5));
+    if(dataLocation != -1) {
+        if(dataLocation > textLocation) dataMemory = dataToMemory(code.slice(dataLocation + 5), code.slice(0, dataLocation + 1).split('\n').length);
+        else                            dataMemory = dataToMemory(code.slice(dataLocation + 5, textLocation), code.slice(0, dataLocation + 1).split('\n').length);
+    }
+    if(dataMemory == -1) return -1;
 
     // if the .globl directive is defined, try to set the startAddress to its label
     let globlLocation = code.search(/\.globl /i);
@@ -37,8 +41,8 @@ function parseCode(code) {
     
     // [labels, memory, pcToLine, exitAddress, startAddress]
     return [
-        labelsInstructions[0],
-        Object.assign({}, dataMemory, labelsInstructions[1]),
+        Object.assign({}, dataMemory[0], labelsInstructions[0]),
+        Object.assign({}, dataMemory[1], labelsInstructions[1]),
         labelsInstructions[2],
         Math.max.apply(null, Object.keys(labelsInstructions[1])),
         startAddress
@@ -59,8 +63,9 @@ function dataToMemory(data, startingLine) {
     let dataMemory = {};
 
     // remove comments
+    let hashtag;
     do {
-        let hashtag = data.indexOf('#');
+        hashtag = data.indexOf('#');
         if(hashtag != -1) {
             let nextline = data.slice(hashtag).indexOf('\n');
             if(nextline != -1) data = data.slice(0, hashtag) + data.slice(nextline + 1);
@@ -69,53 +74,103 @@ function dataToMemory(data, startingLine) {
     } while(hashtag != -1);
 
     // remove strings
+    let doubleQuote;
     do {
-        let doubleQuote = data.indexOf('"');
+        doubleQuote = data.indexOf('"');
         if(doubleQuote != -1) {
-            let secondDoubleQuote = data.slice(doubleQuote).indexOf('"');
-            if(secondDoubleQuote == -1) return syntaxError(data.slice(doubleQuote).split('\n').length);
+            let secondDoubleQuote = data.slice(0, doubleQuote + 1).length + data.slice(doubleQuote + 1).indexOf('"');
+            if(secondDoubleQuote == -1) return syntaxError(startingLine + data.slice(doubleQuote).split('\n').length, 'There is a double quote here which is not matched');
             strings.push(data.slice(doubleQuote + 1, secondDoubleQuote));
-            data = data.slice(0, doubleQuote + 1) + '.STRING' + data.slice(secondDoubleQuote);
-            console.log(doubleQuote, secondDoubleQuote, strings, data)
+            data = data.slice(0, doubleQuote) + '.STRING' + data.slice(secondDoubleQuote + 1);
         }
     } while(doubleQuote != -1);
 
     // read remaining directives and data
-    let tokens = data.split(' ');
+    let tokens = data.replace(/\n/g, ' ').split(' ');
+    console.log(data);
+    console.log(tokens);
     for(let i = 0, tokensLength = tokens.length; i < tokensLength; i++) {
         // skip empty tokens (extra spaces)
         if(tokens[i] == '') continue;
-        // handle strings, each char is loaded into memory as a byte, .asciiz adds a null terminator at the end
-        if(tokens[i] == '.STRING') {
-            if(formatDirective == '.ascii' || formatDirective == '.asciiz') {
-                let str = strings.shift();
-                for(let i = 0, strLength = str.length; i < strLength; i++) memory[address++] = str.charCodeAt(i);
-            }
-            else return syntaxError(10, 'Strings must be loaded into memory using the .ascii or .asciiz directive!', '.asciiz will null terminate the strings for you');
-            if(formatDirective == '.asciiz') {
-
-            }
-        }
         // handle labels
-        let colon = data.indexOf(':');
+        let colon = tokens[i].indexOf(':');
         if(colon != -1) {
             let label = tokens[i].slice(0, colon); // get label before colon
             if(label in labels)                           // duplicate label location error
                 return syntaxError(startingLine + i, 'Two labels cannot have the same name!', label + ': 0x' + (address).toString(16));
             if(Object.values(labels).includes(address))   // duplicate label error
                 return syntaxError(startingLine + i, 'Two labels cannot resolve to the same memory location!', labels[label] + ': 0x' + (address).toString(16));
+            labels[address] = label;
             continue;
         }
+        
+        // handle strings, each char is loaded into memory as a byte, .asciiz adds a null terminator at the end
+        if(tokens[i] == '.STRING') {
+            if(formatDirective == '.ascii' || formatDirective == '.asciiz') {
+                let str = strings.shift();
+                for(let i = 0, strLength = str.length; i < strLength; i++) dataMemory[address++] = str.charCodeAt(i);
+            }
+            else return syntaxError(startingLine + i, 'Strings must be loaded into memory using the .ascii or .asciiz directive!', '.asciiz will null terminate the strings for you');
+            if(formatDirective == '.asciiz') dataMemory[address++] = 0; // insert null terminator
+            continue;
+        }
+
         // handle directive change
-        let period = data.indexOf('.');
+        let period = tokens[i].indexOf('.');
         if(period != -1) {
             let directive = tokens[i].slice(period);
-            if(directive in validDirectives) formatDirective = directive;
+            if(validDirectives.includes(directive)) formatDirective = directive;
             else return syntaxError(startingLine + i, 'This is not a valid directive: ' + tokens[i]);
+            if(directive == '.word') address += address % 4;
+            else if(directive == '.half') address += address % 2;
             continue;
         }
+        
         // all other types of lines
+        if(isNaN(tokens[i])) return syntaxError(startingLine + i, 'Value: ' + parseInt(tokens[i]) + ' does not match directive (it is not a valid integer)');
+        tokens[i] = parseInt(tokens[i]);
+        if((formatDirective == '.word') && (tokens[i] <= 2147483647) && (tokens[i] >= -2147483648)) {
+            dataMemory[address] = tokens[i];
+            address += 4;
+            continue;
+        }
+        if((formatDirective == '.half') && (tokens[i] <= 32767) && (tokens[i] >= -32768)) {
+            console.log("HELLO");
+            if      (address % 4 == 0) {
+                dataMemory[address] &= 0xffff_0000;
+                dataMemory[address] |= (tokens[i] & 0xffff);
+            }
+            else if (address % 4 == 2) {
+                dataMemory[address - 2] &= 0xffff;
+                dataMemory[address - 2] |= (tokens[i] << 16);
+            }
+            address += 2;
+            continue;
+        }
+        if((formatDirective == '.byte') && (tokens[i] <= 255) && (tokens[i] >= -256)) {
+            if      (address % 4 == 0) {
+                dataMemory[address] &= 0xffff_ff00;
+                dataMemory[address] |= (tokens[i] & 0xff);
+            }
+            else if (address % 4 == 1) {
+                dataMemory[address - 1] &= 0xffff_00ff;
+                dataMemory[address - 1] |= (tokens[i] & 0xff) << 8;
+            }
+            else if (address % 4 == 2) {
+                dataMemory[address - 2] &= 0xff00_ffff;
+                dataMemory[address - 2] |= (tokens[i] & 0xff) << 16;
+            }
+            else if (address % 4 == 3) {
+                dataMemory[address - 3] &= 0xff_ffff;
+                dataMemory[address - 3] |= (tokens[i] << 24);
+            }
+            address++;
+            continue;
+        }
+        return syntaxError(lineNumber + i, 'The provided value is not within the range of accepted values!');
     }
+    console.log(labels);
+    console.log(dataMemory);
     return [labels, dataMemory];
 }
 
